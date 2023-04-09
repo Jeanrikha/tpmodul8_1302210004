806fc2cd478ccdb1072d2196df56ee42fbc91cde
Jeanrikha <jeanrikha@student.telkomuniversity.ac.id>
2023-04-10 01:43:41 +0700

    menyelesaikan program dengan runtime configuration

---
+++ CovidConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+
+namespace tpmodul8_1302210004
+{
+    internal class CovidConfig
+    {
+        public string SatuanSuhu { get; set; } = "celcius";
+        public int BatasHariDeman { get; set; } = 14;
+        public string PesanDitolak { get; set; } = "Anda tidak diperbolehkan masuk ke dalam gedung ini";
+        public string PesanDiterima { get; set; } = "Anda dipersilahkan untuk masuk ke dalam gedung ini";
+
+        public void UbahSatuan()
+        {
+            // Mengubah satuan suhu dari celcius ke fahrenheit atau sebaliknya
+            if (SatuanSuhu.ToLower() == "celcius")
+            {
+                SatuanSuhu = "fahrenheit";
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,78 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.Json;
+using tpmodul8_1302210004;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Membaca konfigurasi dari file
+        CovidConfig config = LoadConfig();
+
+        // meminta inputan suhu badan dan lama gejala demam
+        Console.Write($"Berapa suhu badan anda saat ini? Dalam nilai {config.SatuanSuhu}: ");
+        float suhu = float.Parse(Console.ReadLine());
+        Console.Write("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala demam? ");
+        int hariDeman = int.Parse(Console.ReadLine());
+
+        // Menentukan apakah diperbolehkan masuk atau tidak berdasarkan config dan input user
+        if (cekSuhu(config, suhu) && lamaDemam(config, hariDeman))
+        {
+            Console.WriteLine(config.PesanDiterima);
+        }
+        else
+        {
+            Console.WriteLine(config.PesanDitolak);
+        }
+    }
+
+    static CovidConfig LoadConfig()
+    {
+        string configFilePath = "covid_config.json";
+        CovidConfig config;
+
+        if (File.Exists(configFilePath))
+        {
+            // Membaca file config jika sudah ada
+            string json = File.ReadAllText(configFilePath);
+            config = JsonSerializer.Deserialize<CovidConfig>(json);
+        }
+        else
+        {
+            // Menggunakan nilai default jika file config belum ada
+            config = new CovidConfig();
+        }
+
+        return config;
+    }
+
+    static bool cekSuhu(CovidConfig config, float suhu)
+    {
+        // Konversi suhu ke celcius jika inputan suhu adalah fahrenheit
+        if (config.SatuanSuhu.ToLower() == "fahrenheit")
+        {
+            suhu = (suhu - 32) * 5 / 9;
+        }
+
+        // Periksa apakah suhu berada dalam range yang telah ditentukan 
+        if (suhu >= 36.5 && suhu <= 37.5)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    static bool lamaDemam(CovidConfig config, int hariDeman)
+    {
+        // Periksa apakah jumlah hari deman kurang dari data CONFIG2
+        if (hariDeman < config.BatasHariDeman)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
